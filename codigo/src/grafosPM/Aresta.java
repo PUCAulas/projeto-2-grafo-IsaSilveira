3cf090ab77de10c85f162f18bade210ee5b934f1
lucasnmc12 <lucas.nogueira20mc@gmail.com>
2023-10-05 15:53:19 -0300

    Commit Aresta

--- codigo/src/grafosPM/Aresta.java
+++ codigo/src/grafosPM/Aresta.java
@@ -1,5 +1,19 @@
 package grafosPM;
 
-public class Aresta {
+class Aresta {
+    private Cidade destino;
+    private int distancia;
 
+    public Aresta(Cidade destino, int distancia) {
+        this.destino = destino;
+        this.distancia = distancia;
+    }
+
+    public Cidade getDestino() {
+        return destino;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
 }
